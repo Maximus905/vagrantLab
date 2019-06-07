71dfce262720f06454b115928e09995a189f7f17
Karasev DL work <karasev.dmitry@gmail.com>
2019-06-07 18:02:35 +0400

    fix ip addresse masks

--- esxi/proxy.sh
+++ esxi/proxy.sh
@@ -1,4 +1,4 @@
 set http_proxy=http://10.4.68.213:3128
 set https_proxy=%http_proxy%
 set VAGRANT_HTTP_PROXY="%http_proxy%"
-set VAGRANT_NO_PROXY="localhost,127.0.0.1,*.rs.ru"
+set VAGRANT_NO_PROXY="localhost,127.0.0.1,*.rs.ru,192.168.0.0/16,10.0.0.0/8"
